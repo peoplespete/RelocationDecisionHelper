40f97b53196b38b9adc34de125b31057c4d4712c
Pete Dunlap <petedunlap@gmail.com>
2014-01-29 15:33:58 -0600

    unit tests for creating locations complete and clean

--- test/test_entering_locations.rb
+++ test/test_entering_locations.rb
@@ -27,8 +27,6 @@ class TestEnteringLocations < LocationTest
   end
 
   def test_valid_purchase_gets_saved
-  # @@sample_inputs = ['1','Wilmington','DE','2']
-    # `./grocerytracker add Cheerios --calories 210 --price 1.50 --environment test`
     actual = pipe_it @@sample_inputs
     database.results_as_hash = false
     results = database.execute("select * from locations")
@@ -40,53 +38,9 @@ class TestEnteringLocations < LocationTest
     assert_equal 1, result[0][0]
   end
 
-  def test_invalid_purchase_doesnt_get_saved
-    skip
-    `./grocerytracker add Cheerios --calories 210`
-    result = database.execute("select count(id) from purchases")
-    assert_equal 0, result[0][0]
-  end
-
-
-  # def test_valid_purchase_gets_saved
-  #   skip
-
-  #   assert true, "Valid Purchase Saved"
-  # end
-
-  # def test_invalid_purchase_doesnt_get_saved
-  #   skip "needs to be done"
-  #   assert false, "Invalid Purchase Not Saved"
-  # end
-
-  # def test_error_message_for_missing_calories
-  #   skip
-  #   command = './relocator add "Campbells Chicken Soup"'
-  #   expected = "You must provide the caloric value of the product"
-  #   assert_command_output expected, command
-
-  # end
-
-  # def test_error_message_for_missing_name
-  #   skip
-  #   command = './relocator add'
-  #   expected = "You must provide the name of the product"
-  #   assert_command_output expected, command #expected , actual
-  # end
-
 end
 
 
-
-
-  # def test_16month_is_output_first_two_lines
-  #   shell_output = ""
-  #   IO.popen('ruby cal.rb 1 2012', 'r+') do |pipe|
-  #     pipe.close_write
-  #     shell_output = pipe.read
-  #   end
-  #   assert_includes_in_order shell_output, "    January 2012    ", "Su Mo Tu We Th Fr Sa"
-  # end
 
 
 
