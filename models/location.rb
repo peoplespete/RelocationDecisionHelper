60c6f3f589bd6ffc8bb3b7ef5948433dabfed977
Pete Dunlap <petedunlap@gmail.com>
2014-01-29 15:19:23 -0600

    added tests and logic for creating locations in db

--- models/location.rb
+++ models/location.rb
@@ -1,18 +1,40 @@
 require 'highline/import'
 
 class Location
-
-  def initialize
-
+  attr_accessor :name, :state_code, :climate
+  def initialize(options)
+    @name = options[:name]
+    @state_code = options[:state_code].upcase
+    @climate = options[:climate]
   end
 
 
   def self.add
     location_options = {}
     location_options[:name] = ask("What is the name of the city or town you wish to add?") { |q| q.default = "none" }
-    location_options[:state_code] = ask("What state is #{location_options[:name]} in?") { |q| q.default = "none" }
-
-
+    location_options[:state_code] = ask("What state is #{location_options[:name]} in?") { |q| q.validate = /[a-zA-Z][a-zA-Z]/ }
+    choose do |menu|
+      menu.prompt = "What climate does #{location_options[:name]} have?"
+      menu.choice(:Cool) do |chosen|
+        location_options[:climate] = 'Cool'
+      end
+      menu.choice(:Temperate) do |chosen|
+        location_options[:climate] = 'Temperate'
+      end
+      menu.choice(:Warm) do |chosen|
+        location_options[:climate] = 'Warm'
+      end
+    end
+    location = Location.new(location_options)
+    puts location.inspect
+    location.save
   end
+
+  def save
+   database = Environment.database_connection
+   database.execute("insert into locations(city, state_code, climate) values('#{@name}', '#{@state_code}', '#{@climate}')")
+  end
+
+
 
 end
--- test/test_entering_locations.rb
+++ test/test_entering_locations.rb
@@ -6,19 +6,47 @@ class TestEnteringLocations < LocationTest
 # to run a single line test
 #$ ruby test/test_entering_purchases --name test_valid_purchase_gets_saved
 
-  @@sample_inputs = ['1','Wilmington','DE']
+  @@sample_inputs = ['1','Wilmington','DE','2']
 
-  def test_asks_for_name_of_city
+  def test_asks_for_name_of_location
     actual = pipe_it @@sample_inputs
     expected = "What is the name of the city or town you wish to add?"
     assert_includes_in_order actual, expected
   end
 
-  def test_asks_for_state_of_city
+  def test_asks_for_state_of_location
     actual = pipe_it @@sample_inputs
     expected = "What state is Wilmington in?"
     assert_includes_in_order actual, expected
   end
+
+  def test_asks_for_climate_of_location
+    actual = pipe_it @@sample_inputs
+    expected = "What climate does Wilmington have?"
+    assert_includes_in_order actual, expected
+  end
+
+  def test_valid_purchase_gets_saved
+  # @@sample_inputs = ['1','Wilmington','DE','2']
+    # `./grocerytracker add Cheerios --calories 210 --price 1.50 --environment test`
+    actual = pipe_it @@sample_inputs
+    database.results_as_hash = false
+    results = database.execute("select * from locations")
+    expected = ['Wilmington', 'DE', 'Temperate']
+    results[0].shift
+    assert_equal expected, results[0]
+
+    result = database.execute("select count(id) from locations")
+    assert_equal 1, result[0][0]
+  end
+
+  def test_invalid_purchase_doesnt_get_saved
+    skip
+    `./grocerytracker add Cheerios --calories 210`
+    result = database.execute("select count(id) from purchases")
+    assert_equal 0, result[0][0]
+  end
+
 
   # def test_valid_purchase_gets_saved
   #   skip
