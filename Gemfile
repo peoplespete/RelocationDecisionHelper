822394ecb0361c487f046c747814476b45649ec8
Pete Dunlap <petedunlap@gmail.com>
2014-02-03 11:52:13 -0600

    added colorize to give command line some jazz

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "2.0.0"
 
 gem 'sqlite3'
 gem "highline", "~> 1.6.20"
+gem "colorize", "~> 0.6.0"
 # after the gem and name you can specify version by '~> 4.0.0' (means 4.0.above) or '>= 1.2.3' or '=4.2.0'
 
 
--- models/location.rb
+++ models/location.rb
@@ -1,4 +1,8 @@
 require 'highline/import'
+require 'colorize'
+
+$color_dark = :black
+$color_light = :light_yellow
 
 class Location
   attr_accessor :name, :state_code, :climate
@@ -11,28 +15,27 @@ class Location
 
   def self.add
     location_options = {}
-    location_options[:name] = ask("What is the name of the city or town you wish to add?") { |q| q.default = "none" }
+    location_options[:name] = ask("What is the name of the city or town you wish to add?".bold.colorize(:color => $color_light, :background => $color_dark)) { |q| q.default = "none" }
     states = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY","AS","DC","FM","GU","MH","MP","PW","PR","VI","AE","AA","AP"]
-    location_options[:state_code] = ask("What state is #{location_options[:name]} in?") { |q| q.in = (states << states.map{|state| state.downcase}).flatten }
-    # location_options[:state_code] = ask("What state is #{location_options[:name]} in?") { |q| q.validate = /^[a-zA-Z][a-zA-Z]$/ }
+    location_options[:state_code] = ask("What state is #{location_options[:name]} in?".bold.colorize(:color => $color_light, :background => $color_dark)) { |q| q.in = (states << states.map{|state| state.downcase}).flatten }
     choose do |menu|
-      menu.prompt = "What climate does #{location_options[:name]} have?"
-      menu.choice("Cool") do |chosen|
+      menu.prompt = "What climate does #{location_options[:name]} have?".bold.colorize(:color => $color_light, :background => $color_dark)
+      menu.choice("Cool".colorize(:color => $color_light, :background => $color_dark)) do |chosen|
         location_options[:climate] = 'Cool'
       end
-      menu.choice("Temperate") do |chosen|
+      menu.choice("Temperate".colorize(:color => $color_light, :background => $color_dark)) do |chosen|
         location_options[:climate] = 'Temperate'
       end
-      menu.choice("Warm") do |chosen|
+      menu.choice("Warm".colorize(:color => $color_light, :background => $color_dark)) do |chosen|
         location_options[:climate] = 'Warm'
       end
     end
     if find([location_options[:name].capitalize, location_options[:state_code].upcase]) != []
-      puts "Sorry, that location already exists."
+      puts "Sorry, that location already exists.".colorize(:color => $color_dark, :background => $color_light)
     else
       location = Location.new(location_options)
       location.save
-      puts "Location added"
+      puts "Location added".bold.colorize(:color => $color_light, :background => $color_dark)
     end
   end
 
@@ -59,15 +62,15 @@ class Location
     database.results_as_hash = false
     results = database.execute("select city, state_code, climate from locations order by id asc")
     choose do |menu|
-      menu.prompt = "What city would you like to #{action}?"
+      menu.prompt = "What city would you like to #{action}?".bold.colorize(:color => $color_light, :background => $color_dark)
       results.each do |city_state|
-        menu.choice("#{city_state[0]}, #{city_state[1]}") do |chosen|
+        menu.choice("#{city_state[0]}, #{city_state[1]}".colorize(:color => $color_light, :background => $color_dark)) do |chosen|
           city_state
         end
       end
       unless action == 'update'
         all = (action << ' all').upcase
-        menu.choice(all) do |chosen|
+        menu.choice(all.colorize(:color => $color_light, :background => $color_dark)) do |chosen|
           nil
         end
       end
@@ -83,7 +86,7 @@ class Location
       database.execute("delete from locations where id = '#{entry[0]}'")
       plural = 's' if i > 0
     end
-    puts "Location#{plural} Removed"
+    puts "Location#{plural} Removed".bold.colorize(:color => $color_light, :background => $color_dark)
     # database.execute("delete from locations")
   end
 
@@ -91,14 +94,12 @@ class Location
     database = Environment.database_connection
     item_to_update = find([old_data[0],old_data[1]]).flatten
     database.execute("update locations set city = '#{new_data[0].capitalize}', state_code = '#{new_data[1].upcase}', climate = '#{new_data[2].capitalize}' where id = '#{item_to_update[0]}'")
-    puts 'Location Updated'
+    puts 'Location Updated'.colorize(:color => $color_light, :background => $color_dark)
   end
 
   def self.fetch_replacement(old_data)
-    # use map
-    puts old_data.inspect
     old_data.map do |old|
-      ask("The entry is currently: #{old}.  Type a new value or press ENTER to leave unchanged.") { |q| q.default = old }
+      ask("The entry is currently: #{old}.  Type a new value or press ENTER to leave unchanged.".colorize(:color => $color_light, :background => $color_dark)) { |q| q.default = old }
     end
   end
 
